f0969ff2d1c427d3f6ed48172a135c803ce63a2b
Anastasiia2827 <nasty.bond2016@gmail.com>
2023-01-10 23:14:04 +0200

    2task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeElement = document.querySelector('#vimeo-player');
+const player = new Player(iframeElement);
+const currentTime = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+timeVimeo()
+
+function onPlay({ seconds, duration }) {
+    localStorage.setItem(currentTime, seconds);
+    if (seconds === duration) {
+        localStorage.removeItem(currentTime);
+    }
+}
+function timeVimeo() {
+    if (localStorage.getItem(currentTime)) {
+        player.setCurrentTime(localStorage.getItem(currentTime));   
+    }
+}
